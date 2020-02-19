3fc810bceb3fa3ab06dadfc5940dac671b49270e
vamsi <vamsikrishna3415@outlook.in>
2020-02-19 18:48:38 +0530

    setted materalize-css for usage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    // initializing materialize js
+    M.AutoInit();
+
+    //eslint-disable-next-line
+  });
   return <div className='App'>GEt STarting</div>;
 };
 
