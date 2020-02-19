caa7f6152850f42500856fd1a55c7863b0e53763
vamsi <vamsikrishna3415@outlook.in>
2020-02-19 18:13:39 +0530

    upto authcontext setting

---
+++ src/context/auth/AuthState.js
@@ -0,0 +1,120 @@
+import React, { useReducer } from "react";
+import AuthContext from "./authContext";
+import authReducer from "./authReducer";
+import axios from "axios";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  CLEAR_ERRORS,
+  LOGOUT
+} from "../types";
+import setAuthToken from "../../utils/setAuthToken";
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  //LoadUser
+  const loaduser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.get(); // user get url
+      //console.log(res.data);
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    } catch (error) {
+      dispatch({
+        type: AUTH_ERROR,
+        payload: error.response.data.message
+      });
+    }
+  };
+
+  //Register User
+  const register = async formdata => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.post("", formdata, config); //signup url
+      // console.log(res.data);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+      loaduser();
+    } catch (error) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: error.response.data.message
+      });
+    }
+  };
+
+  //LoginUser
+  const login = async formdata => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    //console.log(formdata);
+    try {
+      const res = await axios.post("", formdata, config); //Login Url
+      // console.log(res.data);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+      loaduser();
+    } catch (error) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: error.response.data.message
+      });
+    }
+  };
+
+  //Logout
+  const logout = () => dispatch({ type: LOGOUT });
+  //clearError
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+        register,
+        loaduser,
+        login,
+        logout,
+        clearErrors
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
---
+++ src/context/auth/authReducer.js
@@ -0,0 +1,54 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  CLEAR_ERRORS,
+  LOGOUT
+} from "../types";
+
+export default (state, action) => {
+  switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      };
+
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", action.payload.auth_token);
+      return {
+        ...state,
+        token: action.payload.auth_token,
+        isAuthenticated: true,
+        loading: false
+      };
+
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
