8f368a45e7c29e6ce5a3364c11e3eeea451acf63
vamsi <vamsikrishna3415@outlook.in>
2020-02-20 01:40:07 +0530

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import Home from "./components/pages/Home";
+import AuthState from "./context/auth/AuthState";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   useEffect(() => {
     // initializing materialize js
     M.AutoInit();
-
     //eslint-disable-next-line
   });
-  return <div className='App'>GEt STarting</div>;
+  return (
+    <AuthState>
+      <Router>
+        <div className='App'>GEt STarting</div>
+        <Fragment>
+          <Switch>
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/signup' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </AuthState>
+  );
 };
 
 export default App;
