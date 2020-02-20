34d121881c0dc8f6c1d29e7868c5262e79f79b09
vamsi <vamsikrishna3415@outlook.in>
2020-02-21 00:29:10 +0530

    page to display articles

---
+++ src/components/pages/ArticlesDisplay.js
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect } from "react";
+import ArticleForm from "../articles/ArticleForm";
+import Articles from "../articles/Articles";
+import AuthContext from "../../context/auth/authContext";
+
+const ArticlesDisplay = () => {
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+    authContext.loaduser();
+    //eslint-disable-next-line
+  }, []);
+  return (
+    <div>
+      <Articles />
+    </div>
+  );
+};
+
+export default ArticlesDisplay;
