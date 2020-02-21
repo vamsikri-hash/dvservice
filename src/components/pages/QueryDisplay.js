5d2aa7b5db4294b6be89fa36d8b9dc6c177b34f0
vamsi <vamsikrishna3415@outlook.in>
2020-02-21 17:09:45 +0530

    display page for search bar

---
+++ src/components/pages/QueryDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "../queries/SearchBar";
+
+const QueryDisplay = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default QueryDisplay;
