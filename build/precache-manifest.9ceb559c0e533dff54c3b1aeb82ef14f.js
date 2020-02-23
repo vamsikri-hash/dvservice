48f50270acfafac6fd4245ae1e86cb506a6f3ff7
vamsi <vamsikrishna3415@outlook.in>
2020-02-23 07:48:39 +0530

    depl

---
+++ build/precache-manifest.9ceb559c0e533dff54c3b1aeb82ef14f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e95c1b552afa4f169e79b327111b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf2340c68ac6f518acf",
+    "url": "/static/css/2.b0c6e585.chunk.css"
+  },
+  {
+    "revision": "b4504362c2af4ee78234",
+    "url": "/static/css/main.6c2e8fff.chunk.css"
+  },
+  {
+    "revision": "aaf2340c68ac6f518acf",
+    "url": "/static/js/2.76272b46.chunk.js"
+  },
+  {
+    "revision": "8dd98fd0bd7a29a344e02bb9dfffc3b6",
+    "url": "/static/js/2.76272b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4504362c2af4ee78234",
+    "url": "/static/js/main.41144c50.chunk.js"
+  },
+  {
+    "revision": "6d1a68d317c112efcb24",
+    "url": "/static/js/runtime-main.1fd0e08a.js"
+  },
+  {
+    "revision": "69a32dc8d00d22a7f76fbb9cd035b11f",
+    "url": "/static/media/doc2.69a32dc8.svg"
+  },
+  {
+    "revision": "e83b23839dc63ee206364e62e4bd5a28",
+    "url": "/static/media/help.e83b2383.jpg"
+  },
+  {
+    "revision": "b4d037027ee04685fe3325fcb0c80220",
+    "url": "/static/media/mylogo.b4d03702.svg"
+  }
+]);
